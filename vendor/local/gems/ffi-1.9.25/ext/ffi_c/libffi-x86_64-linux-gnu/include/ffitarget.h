0e2563e419c880e01aa48c1c6968f5cd29a8b51b
titan.net2k <titan.net2k@protonmail.ch>
2018-11-04 19:59:34 +0100

    removed vendor

--- vendor/local/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-linux-gnu/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/home/titannet/dev/stlt_middleman/vendor/local/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/local/gems/middleman-autoprefixer-2.9.0/lib/middleman-autoprefixer/version.rb
+++
@@ -1,5 +0,0 @@
-module Middleman
-  module Autoprefixer
-    VERSION = '2.9.0'.freeze
-  end
-end
